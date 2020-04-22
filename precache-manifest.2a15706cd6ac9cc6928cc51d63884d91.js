b7017b54648d2cc30bb6617835bd7a1cdae51cc1
kim deokhyeon <tosico85@gmail.com>
2020-04-22 12:34:34 +0900

    Updates

---
+++ precache-manifest.2a15706cd6ac9cc6928cc51d63884d91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0600bd10c8fe29536cb2dfafc2a45f38",
+    "url": "/card-manage-app/index.html"
+  },
+  {
+    "revision": "cebeeb77d7bcc28dc512",
+    "url": "/card-manage-app/static/css/main.0397b080.chunk.css"
+  },
+  {
+    "revision": "715f83593a3f34955c3c",
+    "url": "/card-manage-app/static/js/2.faeb868c.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/card-manage-app/static/js/2.faeb868c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cebeeb77d7bcc28dc512",
+    "url": "/card-manage-app/static/js/main.7143de9f.chunk.js"
+  },
+  {
+    "revision": "f3e7e2aeef9aad0d2002",
+    "url": "/card-manage-app/static/js/runtime-main.7e9a6cba.js"
+  }
+]);
